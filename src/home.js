0bd5500e910276168e72c5fd9c6c5b3bbf9e6e2a
RidgeFoo <m.pagnol@pm.me>
2022-06-03 12:29:29 +0100

    Adds module for the home content

---
+++ src/home.js
@@ -0,0 +1,23 @@
+import burger from "./images/burger-no-bg.png";
+
+const home = (function () {
+  const container = document.createElement("div");
+  container.id = "content";
+
+  const backgroundDiv = document.createElement("div");
+  backgroundDiv.className = "background-image";
+
+  const burgerDiv = document.createElement("img");
+  burgerDiv.id = "burger-image";
+  burgerDiv.src = burger;
+
+  const heroMessage = document.createElement("p");
+  heroMessage.id = "hero";
+  heroMessage.textContent = "These burgers\r\n" + "will give you super powers";
+
+  container.append(backgroundDiv, heroMessage, burgerDiv);
+
+  return container;
+})();
+
+export { home };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import "./style.css";
 import { nav, footer } from "./page-load";
-import bg from "./images/bg-splash.jpeg";
-import burger from "./images/burger-no-bg.png";
+import { home } from "./home";
 
+let pageLoaded = false;
 const body = document.querySelector("body");
 const content = document.querySelector("#content");
 
-const backgroundDiv = document.createElement("div");
-backgroundDiv.className = "background-image";
-const burgerDiv = document.createElement("img");
-burgerDiv.id = "burger-image";
-burgerDiv.src = burger;
-
-content.append(burgerDiv);
-content.prepend(backgroundDiv);
-body.prepend(nav);
-
-body.append(footer);
+if (!pageLoaded) {
+  body.prepend(nav);
+  body.append(footer);
+  content.replaceWith(home);
+  pageLoaded = true;
+}
