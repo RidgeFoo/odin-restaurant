46657550723c461041fc109a1cc101af0ceeac68
RidgeFoo <m.pagnol@pm.me>
2022-06-03 13:06:34 +0100

    Moves to using a template HTML file with webpack for the build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
