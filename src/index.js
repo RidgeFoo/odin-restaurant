74f717070e1f5c99bf85e505dd93a69862b9180a
RidgeFoo <m.pagnol@pm.me>
2022-06-06 23:27:31 +0100

    Adds menu functionality and extra pictures for menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { getNavBar, getFooter } from "./page-load";
 import { home } from "./home";
 import { contact } from "./contact";
+import { menu } from "./menu";
 
 const main = (function () {
   const city = "London";
@@ -14,7 +15,7 @@ const main = (function () {
     if (!pageLoaded) {
       const navBar = getNavBar(
         () => updateContent(home),
-        () => content.querySelector(".main").replaceWith(home),
+        () => updateContent(menu()),
         () => updateContent(contact(city, fullAddress))
       );
       content.insertAdjacentElement("afterbegin", navBar);
@@ -24,8 +25,8 @@ const main = (function () {
     }
   }
 
-  function updateContent(elements) {
-    content.querySelector(".main").replaceWith(elements);
+  function updateContent(div) {
+    content.querySelector(".main").replaceWith(div);
   }
 
   setupPage();
--- src/menu.js
+++ src/menu.js
@@ -1,39 +1,90 @@
-const menuItems = function () {
-  const menu = {
-    burgers: [
-      new Burger("Beef", 16.75),
-      new Burger("Fish", 12.5),
-      new Burger("Veggie", 8.5),
-    ],
-    drinks: [
-      new Drink("Cola", 3.5, false),
-      new Drink("Beer", 5.95, true),
-      new Drink("Apple Juice", 3.99, false),
-    ],
-  };
+import beefBurger from "./images/beef-burger.png";
+import fishBurger from "./images/fish-burger.png";
+import veggieBurger from "./images/veggie-burger.png";
+import cola from "./images/cola.png";
+import beer from "./images/beer.png";
+import appleJuice from "./images/apple-juice.png";
 
+const menuItems = (function () {
   class Burger {
-    constructor(type, price) {
+    constructor(type, price, image) {
       this.name = `${type} Burger`;
       this.price = String(price);
       this.description = `A classic ${this.name} with a super twist`;
+      this.image = image;
     }
   }
 
   class Drink {
-    constructor(type, price, isAlcoholic) {
+    constructor(type, price, isAlcoholic, image) {
       this.name = type;
       this.price = price;
       this.isAlcoholic = isAlcoholic;
-      this.description = `A ${this.name} of heroic proportions`;
+      this.description = `${this.name[0].toLowerCase() === "a" ? "An" : "A"} ${
+        this.name
+      } of heroic proportions`;
+      this.image = image;
     }
   }
 
+  const menu = {
+    burgers: [
+      new Burger("Beef", 16.75, beefBurger),
+      new Burger("Fish", 12.5, fishBurger),
+      new Burger("Veggie", 8.5, veggieBurger),
+    ],
+    drinks: [
+      new Drink("Cola", 3.5, false, cola),
+      new Drink("Beer", 5.95, true, beer),
+      new Drink("Apple Juice", 3.99, false, appleJuice),
+    ],
+  };
+
   return menu;
-};
+})();
 
 const menu = (function () {
-  function getElements() {}
+  function getElements() {
+    const container = document.createElement("div");
+    container.className = "main";
+    container.id = "menu";
 
+    const burgers = getMenuCategory("burgers");
+    const drinks = getMenuCategory("drinks");
+
+    container.innerHTML = burgers + "<hr>" + drinks;
+
+    return container;
+  }
+
+  function getMenuCategory(category) {
+    let categoryContainer = `
+    <div class="category">
+      <h1>${category}</h1>
+      ${menuItems[category]
+        .map((i) =>
+          getMenuRow(i.name, i.description, i.image, i.price, i.isAlcoholic)
+        )
+        .join("")}
+    </div>`;
+
+    return categoryContainer;
+  }
+
+  function getMenuRow(name, description, image, price, isAlcoholic) {
+    return `
+    <div class="menu-row">
+      <img src="${image}">
+      <div class="item-description">
+        <h2>${name}</h2>
+        <p>${description}</p>
+        ${isAlcoholic ? "<p>Alcoholic drink</p>" : ""}
+      </div>
+      <div class="price">£${price}</div>
+    </div>
+    `;
+  }
   return getElements;
 })();
+
+export { menu };
